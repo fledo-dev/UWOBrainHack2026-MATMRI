4a6205ae9bd87729d8d9b73e521a6515cae46563
Corey Baron <cbaron4@uwo.ca>
2020-07-20 16:39:52 -0400

    setPath: made a function instead of script

--- setPath.m
+++ setPath.m
@@ -1,24 +1,25 @@
+function setPath
 % Add all subdirectories to path
 %
 % (c) Corey Baron
 
-% Delete any parallel pools, because the workers won't get the new path otherwise
-if(exist('gcp'))
-  poolobj = gcp('nocreate');
-  delete(poolobj);
-  clear poolobj
+  % Delete any parallel pools, because the workers won't get the new path otherwise
+  if(exist('gcp'))
+    poolobj = gcp('nocreate');
+    delete(poolobj);
+    clear poolobj
+  end
+
+  topPath = mfilename('fullpath');
+  topPath = topPath(1:end-length(mfilename));
+
+  % Add paths
+  addpath([topPath, 'bview']);
+  addpath([topPath, 'demos']);
+  addpath([topPath, 'general']);
+  addpath([topPath, 'operators']);
+  addpath([topPath, 'simulation']);
+  addpath([topPath, 'trajectory']);
+  addpath([topPath, 'unitTests']);
+
 end
-
-topPath = mfilename('fullpath');
-topPath = topPath(1:end-length(mfilename));
-
-% Add paths
-addpath([topPath, 'bview']);
-addpath([topPath, 'demos']);
-addpath([topPath, 'general']);
-addpath([topPath, 'operators']);
-addpath([topPath, 'simulation']);
-addpath([topPath, 'trajectory']);
-addpath([topPath, 'unitTests']);
-
-clear topPath 
