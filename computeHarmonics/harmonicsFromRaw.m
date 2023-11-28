8c9adede83388d42f526ef58349f0a49928156b7
Corey Baron <cbaron4@uwo.ca>
2023-11-28 14:46:48 -0500

    minor bug fix for basis function compression

--- computeHarmonics/harmonicsFromRaw.m
+++ computeHarmonics/harmonicsFromRaw.m
@@ -277,15 +277,18 @@ if ~isempty(opt.compMat)
     sz_a = size(phs_spha);
     compMatInv = pinv(opt.compMat);
     % Scale to SI units (compMat uses units of dm)
-    compMatInv(1:5) = 10^2*compMatInv(1:5); % 2nd order
+    compMatInv(1:5,:) = 10^2*compMatInv(1:5,:); % 2nd order
     if size(compMatInv,1) > 5
-        compMatInv(6:12) = 10^3*compMatInv(6:12); % 3nd order
+        compMatInv(6:12,:) = 10^3*compMatInv(6:12,:); % 3nd order
     end
     if size(compMatInv,1) > 12
-        compMatInv(13:21) = 10^4*compMatInv(13:21); % 4th order
+        compMatInv(13:21,:) = 10^4*compMatInv(13:21,:); % 4th order
     end
     if size(compMatInv,1) > 21
-        compMatInv(22:32) = 10^5*compMatInv(22:32); % 5th order
+        compMatInv(22:32,:) = 10^5*compMatInv(22:32,:); % 5th order
+    end
+    if size(compMatInv,1) > 32
+        error('higher than 5th order compression not yet coded!')
     end
     % Uncompress 2nd and higher orders
     phs_spha_a = compMatInv*phs_spha(5:end,:);
